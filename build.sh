275b1195a7052ca301f2532eabeee585c144cd47
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2011-12-21 13:31:42 +0000

    updated jessiecode: parser error output now goes through JessieCode._error, not alert
    git-svn-id: svn://btmdx1.mat.uni-bayreuth.de/jsxgui/JessieCode@454 1ae84437-be93-0410-b691-ab24f1f5886d

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 
 rhino tools/jscc.js -v -w -o jessiecode.bnf.js -p jessie -t tools/driver_jxg.js_ jessiecode.par.bnf
 cat jessiecode.par.js jessiecode.bnf.js > jessiecode.js
+
+cp jessiecode.js ../JSXGraph/src/JessieCode.js
