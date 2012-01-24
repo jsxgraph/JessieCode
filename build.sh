f3cc15bbe8736a323afb9d53af6910ad5cfd63ee
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2012-01-24 09:21:19 +0000

    jessiecode: give parameters the highest priority when resolving variable identifiers
    git-svn-id: svn://btmdx1.mat.uni-bayreuth.de/jsxgui/JessieCode@540 1ae84437-be93-0410-b691-ab24f1f5886d

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-rhino tools/jscc.js -v -w -o jessiecode.bnf.js -p jessie -t tools/driver_jxg.js_ jessiecode.par.bnf
+if test jessiecode.bnf.js -nt jessiecode.js; then
+  echo Recompiling BNF to js...
+  rhino tools/jscc.js -v -w -o jessiecode.bnf.js -p jessie -t tools/driver_jxg.js_ jessiecode.par.bnf
+fi
+
+echo Creating jessiecode.js...
 cat jessiecode.par.js jessiecode.bnf.js > jessiecode.js
 
+echo Copying new version to JSXGraph...
 cp jessiecode.js ../JSXGraph/src/JessieCode.js
